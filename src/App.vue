6a515fda114c7197e5721d953129ab2617abf185
cosmic.gao <cosmic.gao@outlook.com>
2025-11-13 17:42:44 +0800

    Revert "更新代码"
    This reverts commit 75a92fefb90df7ef7377e95925627a43e232f2c8.

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,15 @@
 <template>
-  <div id="id-0">id-0</div>
-  <div id="id-1">id-1</div>
-  <div id="id-2">id-2</div>
-  <div id="p-id-0">p-id-0</div>
+  <div id="1">dom-gen</div>
 </template>
 
 <script lang="ts" setup>
-import DFlexBaseStore, { type RegisterInputProcessed } from "@dflex/store";
-import { onMounted } from "vue";
+import Generator from "@dflex/dom-gen"
 
-class Store extends DFlexBaseStore {
-  register(element: RegisterInputProcessed) {
-    this.addElmToRegistry(element);
-  }
-}
+const domGen = new Generator()
+ domGen.register('id1', 0, false)
+ domGen.register('id3', 0, false)
+ domGen.register('id2', 1, false)
+ domGen.register('id2', 0, false)
 
-const store = new Store();
-
-const elm0DP0 = {
-  id: "id-0",
-  depth: 0,
-  readonly: false,
-  animation: null,
-  CSSTransform: null,
-};
-
-const elm1DP0 = {
-  id: "id-1",
-  depth: 0,
-  readonly: false,
-  animation: null,
-  CSSTransform: null,
-};
-
-const elm2DP0 = {
-  id: "id-2",
-  depth: 0,
-  readonly: false,
-  animation: null,
-  CSSTransform: null,
-};
-
-const elm0DP1 = {
-  id: "p-id-0",
-  depth: 1,
-  readonly: false,
-  animation: null,
-  CSSTransform: null,
-};
-
-onMounted(() => {
-  [elm0DP0, elm1DP0, elm2DP0, elm0DP1].forEach((elm) => {
-    store.register(elm);
-  });
-
-  console.log(store)
-})
+console.log(domGen)
 </script>
